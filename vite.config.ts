1fa6a3a199eb1c21f9a55df4e25bcc612a6f02e3
Sipsam <26bepulwon@gmail.com>
2026-05-13 23:25:09 +0900

    chore(frontend): prepare server-based deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://141.164.48.128:8000',
+                changeOrigin: true,
+                ws: true,
+            },
+        },
+    },
 })
